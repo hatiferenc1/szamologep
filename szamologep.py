d6277c369196de412039baf43bad51c57ad9cc04
hatiferenc1 <72908866+hatiferenc1@users.noreply.github.com>
2023-11-23 15:40:27 +0100

    szamologep.py

--- szamologep.py
+++ szamologep.py
@@ -1,2 +1,45 @@
 import streamlit as st 
-st.write("hello world")
+
+def osszead(elso , masodik):
+  return elso + masodik 
+def kivon(elso , masodik):
+  return elso-masodik
+
+def szorzas(elso , masodik):
+  return elso * masodik 
+def osztas(elso , masodik):
+  if masodik != 0:
+    return elso / masodik
+  else:
+    return "Nullával nem lehet osztani hülyegyerek"
+
+
+def szazalek(szam , szazalek_erteke):
+  return szam * (szazalek_erteke / 100)
+
+def main():
+  st.title("Számológép")
+
+elso = st.number_input("Add meg a második számot:")
+
+muvelet = st.selectbox("Válasszd ki a műveletet ",("összeadás","kivunás","osztás","szorzás","százalékszámítás"))
+
+masodik = st.number_input("Add meg a második számot:")
+
+eredmeny = 0
+if muvelet == "összeadás":
+  result = osszeadas(elso,masodik) 
+elif muvelet == "kivonás":
+  result = kivonas(elso,masodik)
+elif muvelet == "szorzás":
+  result = szorzas(elso,masodik)
+elif muvelet == "osztás":
+  result = osztas(elso,masodik)
+  elif muvelet == "százalékszámítás":
+  result = szazalekszamitas(elso,masodik)
+
+st.write(f"Eredmény:{eredmeny}")
+
+if __name__ == '__main__':
+  main()
+  
